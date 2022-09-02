6b4ff26babdf697b72557a1adc01b1173fe8f558
Ilya <morozovip@mail.ru>
2022-09-03 02:50:43 +0300

    Task 10 - Done

---
+++ seminar_2/task_10/Program.cs
@@ -0,0 +1,15 @@
+﻿
+//создаем рандомные числа c тремя цифрами
+int RandomNum = new Random().Next(100, 1000);
+
+//Создаем функцию/метод 
+int cutMiddleNum(int num)
+{
+    int firstNum = num % 100;
+    int middleNum = firstNum / 10;
+    return middleNum;
+}
+
+//выводим рандомное число и результат метода
+Console.WriteLine(RandomNum);
+Console.WriteLine(cutMiddleNum(RandomNum));
