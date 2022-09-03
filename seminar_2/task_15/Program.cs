f124774a0f4eafe20021a02106a3c0461ef516e1
Ilya <morozovip@mail.ru>
2022-09-03 14:26:18 +0300

    15 task - Done

---
+++ seminar_2/task_15/Program.cs
@@ -0,0 +1,16 @@
+﻿int randomNum = new Random().Next(1, 8);//Генерируем любое число от 1 до 8
+int[] weekend = { 6, 7 };//создаем массив с выходными днями
+
+void itsWeekend(int num)//создаем метод/функцию с проверкой дня на выходной
+{
+    if (num == weekend[0] || num == weekend[1])
+    {
+        Console.WriteLine("Да, выходной");
+    }
+    else
+    {
+        Console.WriteLine("Нет, не выходной");
+    }
+}
+Console.WriteLine(randomNum);//выводим день
+itsWeekend(randomNum);//выводим результат проверки
