e0ca6c2fe76582bd23b44a565ff27aa9663f49a9
Ilya <morozovip@mail.ru>
2022-09-10 21:50:27 +0300

    task 2 Done

---
+++ seminar_4/task_2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumNum(int number)
+{
+    int count = Convert.ToString(number).Length;
+    int temp = 0;
+    int result = 0;
+
+    for (int i = 0; i < count; i++)
+    {
+        temp = number % 10;
+        result = result + temp;
+        number = number / 10;
+    }
+    return result;
+}
+Console.WriteLine($"Сумма цифр в числе = {SumNum(number)}");
