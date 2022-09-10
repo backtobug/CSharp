96070d734ef9f952e7d8c8b9d09a8998aacc9cc9
Ilya <morozovip@mail.ru>
2022-09-11 00:06:07 +0300

    task 3 Done

---
+++ seminar_4/task_3/Program.cs
@@ -0,0 +1,23 @@
+﻿
+int[] Createarray()
+{
+    Random rnd = new Random();
+    int[] array = new int[8];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(0, 100);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+int[] arr = Createarray();
+Console.WriteLine(arr.Length);
+PrintArray(arr);
