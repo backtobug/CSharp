d332f03e399e5df744a46ac7150755379001e32c
Ilya <morozovip@mail.ru>
2022-09-10 21:29:10 +0300

    task 1 Done

---
+++ seminar_4/task_1/Program.cs
@@ -0,0 +1,17 @@
+﻿int GetDegree(int numberA, int numberB)
+{
+    int result = 1;
+    for (int i = 1; i <= numberB; i++)
+    {
+        result = result * numberA;
+    }
+    return result;
+}
+
+Console.Write("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int degree = GetDegree(numberA, numberB);
+Console.WriteLine($"{numberA} в степени {numberB} равно: {degree}");
