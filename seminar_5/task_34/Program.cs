fca999747a3a9ae9c9930dd3ddb97296930024e8
Ilya <morozovip@mail.ru>
2022-09-14 11:40:33 +0300

    task 34 Done

---
+++ seminar_5/task_34/Program.cs
@@ -0,0 +1,36 @@
+﻿
+int[] array = new int[4];
+
+
+void GetRandomArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"{i + 1} Число в массиве {array[i]}");
+    }
+}
+
+void FindEven(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine($"Колличество четных чисел в массиве: {count}");
+}
+
+GetRandomArray(array);
+PrintArray(array);
+FindEven(array);
