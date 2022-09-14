8034174ac48f904ccb6a62b6626af2e91c1c269f
Ilya <morozovip@mail.ru>
2022-09-14 12:00:28 +0300

    task 36 Done

---
+++ seminar_5/task_36/Program.cs
@@ -0,0 +1,33 @@
+﻿int[] array = new int[4];
+
+void GetRandomArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-100, 100);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"{i} индекс в массиве {array[i]}");
+    }
+}
+
+void SumOfOddIndices(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            sum += array[i];
+        }
+    }
+    Console.WriteLine($"Сумма чисел в нечетных индексах: {sum}");
+}
+GetRandomArray(array);
+PrintArray(array);
+SumOfOddIndices(array);
