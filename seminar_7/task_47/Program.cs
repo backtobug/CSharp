0f45accc92cca8344fd9d98b37095241e8576006
Ilya <morozovip@mail.ru>
2022-09-20 23:26:08 +0300

    task 47 Done

---
+++ seminar_7/task_47/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Write("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] array = new double[m, n];
+
+CreateArrayDouble(array);
+ShowArray(array);
+
+void CreateArrayDouble(double[,] array)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = new Random().NextDouble() * 20 - 10;
+        }
+    }
+}
+
+void ShowArray(double[,] array)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            double Number = Math.Round(array[i, j], 1);
+            Console.Write(Number + " ");
+        }
+        Console.WriteLine();
+    }
+}
