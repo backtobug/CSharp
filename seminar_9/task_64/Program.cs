67667d2ddceae68aa0e4da32a66cd3f4d5207503
Ilya <morozovip@mail.ru>
2022-09-28 13:53:58 +0300

    task 64 Done

---
+++ seminar_9/task_64/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+NaturalNumber(number, count);
+
+void NaturalNumber(int n, int count)
+{
+    if (count > n)
+    {
+        return;
+    }
+    else
+    {
+        NaturalNumber(n, count + 1);
+        Console.Write(count + " ");
+    }
+}
