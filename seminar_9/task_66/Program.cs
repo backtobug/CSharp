002ef878f611d7f682fb2cf326623b9321db08da
Ilya <morozovip@mail.ru>
2022-09-28 14:03:39 +0300

    task 66 Done

---
+++ seminar_9/task_66/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+SumForMN(m, n);
+
+void SumForMN(int m, int n)
+{
+    Console.Write(SumMN(m - 1, n));
+}
+
+int SumMN(int m, int n)
+{
+    int result = m;
+    if (m == n)
+        return 0;
+    else
+    {
+        m++;
+        result = m + SumMN(m, n);
+        return result;
+    }
+}
